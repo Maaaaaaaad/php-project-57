3cf6cdce9af31e3d3ae1def118cda8625452b256
Maaaaaaaad <wowa-iff@yandex.ru>
2024-11-07 21:59:45 +0300

    fix index.php

--- public/index.php
+++ public/index.php
@@ -22,6 +22,6 @@ if (file_exists(__DIR__ . '/../' . '.env'))
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../', '.env');
     $dotenv->load();
 } else{
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . 'etc/setcrest/../', '.env');
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/etc/secrets/', '.env');
     $dotenv->load();
 }
