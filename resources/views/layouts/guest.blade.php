1c638cb9363737f2f889bee323d73b7dab5374a8
Maaaaaaaad <wowa-iff@yandex.ru>
2024-11-08 14:48:50 +0300

    fix phpunit.xml

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,11 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Task Manager</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -19,7 +19,6 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
                 <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                     <h2 class="text-center"><a href="https://php-task-manager-ru.hexlet.app">Менеджер задач</a></h2>
-
                     {{ $slot }}
                 </div>
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Менеджер задач</title>
+        <title>Task Manager</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"/>
 
         <!-- Styles / Scripts -->
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
@@ -24,41 +24,50 @@
             <header class="fixed w-full">
                 <nav class="bg-white border-gray-200 py-2.5 dark:bg-gray-900 shadow-md">
                     <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
-                        <a href="/" class="flex items-right">
+                        <a href="{{ url('/') }}" class="flex items-center">
                             <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Менеджер задач</span>
                         </a>
                         <div class="flex items-center lg:order-2">
                         @if (Route::has('login'))
-                                <nav class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
                         @auth
-                                <div class="flex items-center lg:order-2">
-                                   <a href="{{ url('/dashboard') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Dashboard</a>
-                         @else
-                               <div class="flex items-center lg:order-2">
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+
+                                        <x-dropdown-link :href="route('logout')"
+                                                         onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                            {{ __('Log Out') }}
+                                        </x-dropdown-link>
+                                    </form>
+                                @else
                                 <a href="{{ route('login') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Вход</a>
                          @if (Route::has('register'))
                                 <a href="{{ route('register') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">Регистрация</a>
                             </div>
+
                          @endif
                          @endauth
-                            </nav>
+                                </div>
+
                         </div>
                         @endif
-
-                        <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                            <li>
-                                <a href="" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">Задачи</a>
-                            </li>
-                            <li>
-                                <a href="" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">Статусы</a>
-                            </li>
-                            <li>
-                                <a href="" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">Метки</a>
-                            </li>
-                        </ul>
-                    </div>
-        </div>
-                </nav>
+                            <div class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1">
+                                <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                                    <li>
+                                        <a href="" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
+                                            Задачи                                </a>
+                                    </li>
+                                    <li>
+                                        <a href="" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
+                                            Статусы                                </a>
+                                    </li>
+                                    <li>
+                                        <a href="" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
+                                            Метки                                </a>
+                                    </li>
+                                </ul>
+                            </div>
+               </nav>
             </header>
 
             <section class="bg-white dark:bg-gray-900">
