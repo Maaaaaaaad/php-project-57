7f613864f1b549496f9b733f73d2d793776a1b6d
Maaaaaaaad <wowa-iff@yandex.ru>
2024-11-10 16:50:00 +0300

    LoginRequest.php index.php fix

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 
 
+
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
@@ -13,17 +14,18 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
-
 if (file_exists(__DIR__ . '/../' . '.env'))
 {
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../', '.env');
     $dotenv->load();
 } else{
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../etc/secrets/', '.env');
-
-    dump();
     $dotenv->load();
 }
+
+// Bootstrap Laravel and handle the request...
+(require_once __DIR__.'/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
+
+
+
