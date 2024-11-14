3a9c208f7ce39dd7bc8d207919bdb8010272cf81
Maaaaaaaad <wowa-iff@yandex.ru>
2024-11-14 12:53:49 +0300

    fix tests

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -24,6 +24,7 @@ class LoginRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
+     *
      * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
      */
     public function rules(): array
