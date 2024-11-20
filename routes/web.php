eed7671aa4dcd79febe27f890ab2c4f13ab7c3d1
Maaaaaaaad <wowa-iff@yandex.ru>
2024-11-20 20:39:28 +0300

    fix auth

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 Route::get('task_statuses', [StatusController::class, 'index'])
     ->name('statuses');
 Route::get('task_statuses/create', [StatusController::class, 'create'])
@@ -61,7 +51,6 @@ Route::get('tasks/{id}/edit', [TasksController::class, 'edit'])
     ->name('task.edit');
 Route::post('tasks/{id}/edit', [TasksController::class, 'edit'])
     ->name('task.edit');
-
 
 
 Route::get('labels', [LabelsController::class, 'index'])
