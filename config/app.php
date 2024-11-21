162280d1064352a311dfccc42fc8cc3360189f18
Maaaaaaaad <wowa-iff@yandex.ru>
2024-11-21 12:40:35 +0300

    Add ru lang

--- config/app.php
+++ config/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use LaravelLang\LocaleList\Locale;
+
 return [
 
     /*
@@ -78,11 +80,11 @@ return [
     |
     */
 
-    'locale' => env('APP_LOCALE', 'en'),
+    'locale' => Locale::Russian->value,
 
-    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'ru'),
 
-    'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
+    'faker_locale' => env('APP_FAKER_LOCALE', 'ru_RU'),
 
     /*
     |--------------------------------------------------------------------------
