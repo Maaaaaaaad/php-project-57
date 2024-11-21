e2fb608259f7c67b50ffdab9c027cd996c5df001
Maaaaaaaad <wowa-iff@yandex.ru>
2024-11-21 14:43:11 +0300

    Fix composer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,5 +18,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->isLocal()) {
+
+        } else {
+            URL::forceScheme('https');
+        }
     }
 }
